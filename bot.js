73f8c701a27f62edc326edc7d6fd0753b3da1161
unknown <antoine.b44@outlook.fr>
2020-04-26 19:20:34 +0200

    Upgrade the IA
    The bot now bloc the player from finishing his line

--- bot.js
+++ bot.js
@@ -1,11 +1,14 @@
 var twit = require('twit');
 var client = new twit(require('./config'));
 
+// API
 var streamStart = client.stream('statuses/filter', {track: '@MipikerLeGrand morpion ?'});
 streamStart.on('tweet', function (tweet) {
+	// Starting
 	var userScreenName = '@' + tweet.user.screen_name;
-	console.log("Starting a new game with " + userScreenName);
+	console.log('Starting a new game with ' + userScreenName);
   	client.post('statuses/update', {status:  userScreenName + ' ok, I play pawn X and you O'});
+  	// Playing against player
  	var mat =	[['▢', '▢', '▢'],
 				 ['▢', '▢', '▢'],
 				 ['▢', '▢', '▢']];
@@ -28,7 +31,7 @@ streamStart.on('tweet', function (tweet) {
 				x = 0;
 				y++;
 			}
-			if(mat[x][y] === '▢' && msg[j] != '▢') {
+			if(mat[x][y] === '▢' && msg[j] !== '▢') {
 				mat[x][y] = msg[j];
 				break;
 			}
@@ -40,27 +43,97 @@ streamStart.on('tweet', function (tweet) {
 	});
 });
 
+// Place bot pawn
 function play(mat, pawn) {
-	// Bloc player strategy
 	var bestX = -1;
 	var bestY = -1;
-	var nbO = 0;
+	var bestNbOponentCouldStop = 0;
+	stopSearch:
 	for(var y = 0; y < 3; y++) {
 		for(var x = 0; x < 3; x++) {
 			if(mat[x][y] ==='▢') {
-				var actNbO = 0;
+				var nbOponentCouldStop = 0;
+				var nbOponentCouldStopLine = 0;
+				var alreadyBlockedLine = false;
+				// Line
 				for(var x_ = 0; x_ < 3; x_++) {
-					if(x_ != x && mat[x_][y] != pawn && mat[x_][y] != '▢') {
-						actNbO++;
+					if(x_ !== x) {
+						if(mat[x_][y] !== pawn && mat[x_][y] !== '▢') {
+							nbOponentCouldStop++;
+							nbOponentCouldStopLine++;
+						}
+						if(mat[x_][y] === pawn) {
+							alreadyBlockedLine = true;
+						}
 					}
 				}
+				if(!alreadyBlockedLine && nbOponentCouldStopLine === 2) {
+					bestX = x;
+					bestY = y;
+					break stopSearch;
+				}
+				// Column
+				nbOponentCouldStopLine = 0;
 				for(var y_ = 0; y_ < 3; y_++) {
-					if(y_ != y && mat[x][y_] != pawn && mat[x][y_] != '▢') {
-						actNbO++;
+					if(x_ !== x) {
+						if(mat[x][y_] !== pawn && mat[x][y_] !== '▢') {
+							nbOponentCouldStop++;
+							nbOponentCouldStopLine++;
+						}
+						if(mat[x][y_] === pawn) {
+							alreadyBlockedLine = true;
+						}
 					}
 				}
-				if(actNbO > nbO) {
-					nbO = actNbO;
+				if(!alreadyBlockedLine && nbOponentCouldStopLine === 2) {
+					bestX = x;
+					bestY = y;
+					break stopSearch;
+				}
+				// Diagonal \
+				nbOponentCouldStopLine = 0;
+				if(x === y) {
+					for(var i = 0; i < 3; i++) {
+						if(i !== x) {
+							if(mat[i][i] !== pawn && mat[i][i] !== '▢') {
+								nbOponentCouldStop++;
+								nbOponentCouldStopLine++;
+							}
+							if(mat[i][i] === pawn) {
+								alreadyBlockedLine = true;
+							}
+						}
+					}
+				}
+				if(!alreadyBlockedLine && nbOponentCouldStopLine === 2) {
+					bestX = x;
+					bestY = y;
+					break stopSearch;
+				}
+				// Diagonal /
+				nbOponentCouldStopLine = 0;
+				if(2 - x === y) {
+					for(var i = 0; i < 3; i++) {
+						if(i !== y) {
+						 	if(mat[2 - i][i] !== pawn && mat[2 - i][i] !== '▢') {
+								nbOponentCouldStop++;
+								nbOponentCouldStopLine++;
+							}
+							if(mat[2 - i][i] === pawn) {
+								alreadyBlockedLine = true;
+							}
+						}
+					}	
+				}
+				if(!alreadyBlockedLine && nbOponentCouldStopLine === 2) {
+					bestX = x;
+					bestY = y;
+					break stopSearch;
+				}
+
+				// If the bot can't lose, he play the place where it bloc the max nb of pawn of his opponent
+				if(nbOponentCouldStop > bestNbOponentCouldStop) {
+					bestNbOponentCouldStop = nbOponentCouldStop;
 					bestX = x;
 					bestY = y;
 				}
@@ -68,13 +141,73 @@ function play(mat, pawn) {
 		}
 	}
 
-	if(bestX === -1 && bestY === -1) {
-		pickRandomly(mat, pawn);
-	} else {
+	if(bestX !== -1 && bestY !== -1) {
 		mat[bestX][bestY] = pawn;
+	} else { // Happen when nobody has played when the game start because thre is no best move
+		pickRandomly(mat, pawn);
 	}
 }
 
+// Detect if somebody won
+function detectWin(mat) {
+	// Lines
+	for(var y = 0; y < 3; y++) {
+		var X = 0;
+		var O = 0;
+		for(var x = 0; x < 3; x++) {
+			if(mat[x][y] === 'X') {
+				X++;
+			} else if(mat[x][y] === 'Y') {
+				Y++;
+			}
+		}
+		if(X === 3) {
+			return true;
+		} else if (O === 3) {
+			return true;
+		}
+	}
+	// Columns
+	for(var x = 0; x < 3; x++) {
+		var X = 0;
+		var O = 0;
+		for(var y = 0; y < 3; y++) {
+			if(mat[x][y] === 'X') {
+				X++;
+			} else if(mat[x][y] === 'Y') {
+				Y++;
+			}
+		}
+		if(X === 3) {
+			return true;
+		} else if (O === 3) {
+			return true;
+		}
+	}
+	// Diagonal
+	var X1 = 0;
+	var O1 = 0;
+	var X2 = 0;
+	var O2 = 0;
+	for(var i = 0; i < 3; i++) {
+		if(mat[i][i] === 'X') {
+			X1++;
+		} else if(mat[i][i] === 'Y') {
+			Y1++;
+		}
+		if(mat[2 - i][i] === 'X') {
+			X2++;
+		} else if(mat[2 - i][i] === 'Y') {
+			Y2++;
+		}
+	}
+	if(X1 === 3 || O1 === 3 || X2 === 3 || O2 === 3) {
+		return true;
+	}
+	return false;
+}
+
+// Return a string that represent the given matrix
 function display(mat) {
 	var display = '';
 	for(var y = 0; y < 3; y++) {
@@ -87,6 +220,7 @@ function display(mat) {
 	return display;
 }
 
+// Put the given pawn randomly in the matrix, it can be placed only if the matrix contain a least one '▢'
 function pickRandomly(mat, pawn) {
 	var find = false;
 	do {
@@ -99,6 +233,26 @@ function pickRandomly(mat, pawn) {
 	} while(!find);
 }
 
+// Return a random integer, 0 include, max not include
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+
+/*var mat =	[['▢', '▢', 'O'],
+			 ['▢', 'O', '▢'],
+			 ['▢', '▢', '▢']];
+for(var i = 0; i < 4; i++) {
+	play(mat, 'X');
+	console.log(display(mat));
+	if(detectWin(mat)) {
+		console.log('X win');
+		break;
+	}
+	play(mat, 'O');
+	console.log(display(mat));
+	if(detectWin(mat)) {
+		console.log('O win');
+		break;
+	}
+}
+console.log('No win');*/
